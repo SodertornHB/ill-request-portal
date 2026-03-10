d0f2b8075e1ebdf6830f5c2bc1279b6415f39caf
Stefan Norinder <stefan.norinder@sh.se>
2026-03-10 10:58:19 +0100

    fix bug. isbn not saved i datasource

--- IllRequestPortal.Web/ViewModel/IllRequestViewModel.cs
+++ IllRequestPortal.Web/ViewModel/IllRequestViewModel.cs
@@ -11,18 +11,33 @@ namespace IllRequestPortal.Web.ViewModel
 {
     public partial class IllRequestViewModel : ViewModelBase
     {
-        public virtual string Title {get;set;}  = ""; 
+        [Required(ErrorMessage = "TitleRequired")]
+        public string Title { get; set; } = "";
+
+        [Required(ErrorMessage = "IsbnIssnRequired")]
+        [MinLength(8, ErrorMessage = "IsbnIssnTooShort")]
+        public string IsbnIssn { get; set; } = "";
+
+        [Required(ErrorMessage = "MaterialTypeRequired")]
+        public string MaterialType { get; set; } = "";
+
+        [Required(ErrorMessage = "RequestTypeRequired")]
+        public string RequestType { get; set; } = "";
+
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "CardNumberMustBe10Digits")]
+        public string CardNumber { get; set; } = "";
+
+        [Required(ErrorMessage = "RequesterNameRequired")]
+        public string RequesterName { get; set; } = "";
+
+        [Required(ErrorMessage = "RequesterEmailRequired")]
+        [EmailAddress(ErrorMessage = "RequesterEmailInvalid")]
+        public string RequesterEmail { get; set; } = "";
         public virtual string Author {get;set;}  = ""; 
         public virtual string PublicationYear {get;set;}  = ""; 
         public virtual string Edition {get;set;}  = "";
-        public virtual string IsbnIssn { get; set; } = "";
         public virtual string Isbn { get; set; } = "";
         public virtual string Issn {get;set;}  = ""; 
-        public virtual string MaterialType {get;set;}  = ""; 
-        public virtual string RequestType {get;set;}  = ""; 
-        public virtual string RequesterName {get;set;}  = ""; 
-        public virtual string RequesterEmail {get;set;}  = ""; 
-        public virtual string CardNumber {get;set;}  = ""; 
         public virtual string Status {get;set;}  = ""; 
         public virtual string ExternalRequestId {get;set;}  = ""; 
         [DataType(DataType.Text)]
--- IllRequestPortal.Web/ViewModel/IllRequestViewModelExtended.cs
+++ IllRequestPortal.Web/ViewModel/IllRequestViewModelExtended.cs
@@ -1,37 +1,9 @@
-using System.ComponentModel.DataAnnotations;
 using static Logic.Util.StandardNumberUtil;
 
 namespace IllRequestPortal.Web.ViewModel
 {
     public partial class IllRequestViewModelExtended : IllRequestViewModel
     {
-
-
-        [Required(ErrorMessage = "TitleRequired")]
-        public new string Title { get; set; } = "";
-
-        [Required(ErrorMessage = "IsbnIssnRequired")]
-        [MinLength(8, ErrorMessage = "IsbnIssnTooShort")]
-        public new string IsbnIssn { get; set; } = "";
-
-        [Required(ErrorMessage = "MaterialTypeRequired")]
-        public new string MaterialType { get; set; } = "";
-
-        [Required(ErrorMessage = "RequestTypeRequired")]
-        public new string RequestType { get; set; } = "";
-
-        [Required(ErrorMessage = "CardNumberRequired")]
-        [RegularExpression(@"^\d{10}$", ErrorMessage = "CardNumberMustBe10Digits")]
-        public new string CardNumber { get; set; } = "";
-
-        [Required(ErrorMessage = "RequesterNameRequired")]
-        public new string RequesterName { get; set; } = "";
-
-        [Required(ErrorMessage = "RequesterEmailRequired")]
-        [EmailAddress(ErrorMessage = "RequesterEmailInvalid")]
-        public new string RequesterEmail { get; set; } = "";
-
-
         public override string Isbn
         {
             get
