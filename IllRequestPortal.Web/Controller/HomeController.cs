c4b4c7d928b159d44a8f7fc92e8a0a758445621a
Stefan Norinder <stefan.norinder@sh.se>
2026-03-24 08:27:24 +0100

    clear form data

--- IllRequestPortal.Web/Controller/HomeController.cs
+++ IllRequestPortal.Web/Controller/HomeController.cs
@@ -1,14 +1,9 @@
-
-//--------------------------------------------------------------------------------------------------------------------
-// Warning! This is an auto generated file. Changes may be overwritten. 
-// Generator version: 0.0.1.0
-//-------------------------------------------------------------------------------------------------------------------- 
-
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Sh.Library.Authentication;
 using System.Globalization;
 using System.Linq;
 using System.Threading;
@@ -24,6 +19,7 @@ namespace Web.Controllers
             this.localizationOptions = localizationOptions.Value;
         }
 
+        [NoLibraryAuth]
         public IActionResult Index()
         {
             return RedirectToAction("Create", "IllRequest");
@@ -35,6 +31,7 @@ namespace Web.Controllers
             return RedirectToAction("Index", "IllRequest");
         }
 
+        [NoLibraryAuth]
         [HttpPost]
         public IActionResult ToggleCulture(string returnUrl)
         {
@@ -42,14 +39,16 @@ namespace Web.Controllers
             SetCookie(culture);
 
             return Redirect(returnUrl);
-        }        
+        }
 
+        [NoLibraryAuth]
         [HttpGet("error")]
         public IActionResult Error()
         {
             return View();
         }
 
+        [NoLibraryAuth]
         [HttpGet("no-auth")]
         public IActionResult NoAuth()
         {
